b8d8e3638c8d4738b497d025970317f37543ee12
UChLab <rogerdom@gmail.com>
2016-05-27 15:47:06 +0100

    Changed typed tween version from clamted to unclamted lerp functions to allow elasctic tweens

--- src/LerpFunction.cs
+++ src/LerpFunction.cs
@@ -4,7 +4,7 @@ namespace Between {
     public static class LerpFunction {
 
         public static int IntLerp(int start, int end, float percent) {
-            return Mathf.FloorToInt(Mathf.Lerp((float)start, (float)end, percent));
+            return Mathf.FloorToInt(Mathf.LerpUnclamped((float)start, (float)end, percent));
         }
     }
 }
--- src/unity/TypedTweens.cs
+++ src/unity/TypedTweens.cs
@@ -8,7 +8,7 @@ namespace Between.Unity {
     }
 
     public class FloatTween : Tween<float> {
-        public FloatTween(float start, float end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Mathf.Lerp, easeFunc) {
+        public FloatTween(float start, float end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Mathf.LerpUnclamped, easeFunc) {
         }
     }
 
@@ -19,23 +19,23 @@ namespace Between.Unity {
 
     public class Vector2Tween : Tween<Vector2> {
 
-        public Vector2Tween(Vector2 start, Vector2 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector2.Lerp, easeFunc) {
+        public Vector2Tween(Vector2 start, Vector2 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector2.LerpUnclamped, easeFunc) {
         }
     }
 
     public class Vector3Tween : Tween<Vector3> {
 
-        public Vector3Tween(Vector3 start, Vector3 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector3.Lerp, easeFunc) {
+        public Vector3Tween(Vector3 start, Vector3 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector3.LerpUnclamped, easeFunc) {
         }
     }
 
     public class Vector4Tween : Tween<Vector4> {
-        public Vector4Tween(Vector4 start, Vector4 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector4.Lerp, easeFunc) {
+        public Vector4Tween(Vector4 start, Vector4 end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Vector4.LerpUnclamped, easeFunc) {
         }
     }
 
     public class QuaternionTween : Tween<Quaternion> {
-        public QuaternionTween(Quaternion start, Quaternion end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Quaternion.Lerp, easeFunc) {
+        public QuaternionTween(Quaternion start, Quaternion end, float duration, Between.EaseFunc easeFunc = null) : base(start, end, duration, Quaternion.LerpUnclamped, easeFunc) {
         }
     }
 
